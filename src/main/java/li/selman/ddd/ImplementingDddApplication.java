524ef7a272bc7db237c4499b8481f506f67191c4
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2024-06-26 21:01:48 +0200

    Add @Modlith annotation to main class

--- src/main/java/li/selman/ddd/ImplementingDddApplication.java
+++ src/main/java/li/selman/ddd/ImplementingDddApplication.java
@@ -2,8 +2,10 @@ package li.selman.ddd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.modulith.Modulith;
 
 @SpringBootApplication
+@Modulith
 public class ImplementingDddApplication {
 
     public static void main(String[] args) {
