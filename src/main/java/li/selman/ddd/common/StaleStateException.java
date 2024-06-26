3ff3e3e64b45405fbfbf068eb34abdc63b94b4c4
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2024-06-26 21:00:07 +0200

    Move StaleStateException to commons module

---
+++ src/main/java/li/selman/ddd/common/StaleStateException.java
@@ -0,0 +1,15 @@
+package li.selman.ddd.common;
+
+import org.jmolecules.ddd.types.Identifier;
+
+public class StaleStateException extends RuntimeException {
+
+    private StaleStateException(String id) {
+        super(String.format("Aggregate of id %s is stale", id));
+    }
+
+    public static StaleStateException forAggregate(Identifier identifier) {
+        return new StaleStateException(identifier.toString());
+    }
+
+}
