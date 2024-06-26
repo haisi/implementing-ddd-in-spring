9563b214db10c22f1dc705e407987c03d6a70264
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2024-06-26 21:01:31 +0200

    Expand domain

--- src/main/java/li/selman/ddd/statement/Reaction.java
+++ src/main/java/li/selman/ddd/statement/Reaction.java
@@ -1,16 +1,35 @@
 package li.selman.ddd.statement;
 
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
 import org.jmolecules.ddd.types.Entity;
 import org.jmolecules.ddd.types.Identifier;
 
+@jakarta.persistence.Entity
 public class Reaction implements Entity<Statement, Reaction.ReactionId> {
 
+    @EmbeddedId
+    @SuppressWarnings("NullAway.Init")
     private ReactionId id;
+
+    protected Reaction() {
+        // For JPA
+    }
+
+    public Reaction(ReactionId id) {
+        this.id = id;
+    }
 
     @Override
     public ReactionId getId() {
         return id;
     }
 
-    public record ReactionId(String value) implements Identifier {}
+    public enum Type {
+        VERBAL, COMMENT
+    }
+
+    @Embeddable
+    public record ReactionId(String value) implements Identifier {
+    }
 }
--- src/main/java/li/selman/ddd/statement/Statement.java
+++ src/main/java/li/selman/ddd/statement/Statement.java
@@ -1,12 +1,12 @@
 package li.selman.ddd.statement;
 
-import jakarta.persistence.AttributeOverride;
-import jakarta.persistence.Column;
-import jakarta.persistence.Version;
+import jakarta.persistence.*;
 import li.selman.ddd.statement.Reaction.ReactionId;
 import org.jmolecules.ddd.types.AggregateRoot;
 import org.jmolecules.ddd.types.Identifier;
+import org.jmolecules.ddd.types.ValueObject;
 import org.jmolecules.event.types.DomainEvent;
+import org.jmolecules.jpa.JMoleculesJpa;
 import org.springframework.data.domain.AbstractAggregateRoot;
 
 import java.time.LocalDate;
@@ -16,26 +16,65 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ *
+ */
+@SuppressWarnings("NullAway.Init")
+@Entity
 public class Statement
         extends AbstractAggregateRoot<Statement>
         implements AggregateRoot<Statement, Statement.StatementId> {
 
-    @AttributeOverride(name = "id", column = @Column(name = "id"))
+    @EmbeddedId
+    @AttributeOverride(name = "date", column = @Column(name = "made_at"))
+    @AttributeOverride(name = "type", column = @Column(name = "type"))
+    @AttributeOverride(name = "sequenceOfDay", column = @Column(name = "sequence_of_day"))
     private StatementId id;
 
     @Version
+    @SuppressWarnings("unused")
     private Long version;
 
+    @Enumerated(EnumType.STRING)
     private State state;
 
+    @AttributeOverride(name = "value", column = @Column(name = "author_id"))
+    private AuthorId authorId;
+
+    @OneToMany(
+            fetch = FetchType.EAGER,
+            orphanRemoval = true,
+            cascade = {CascadeType.ALL},
+            mappedBy = ""
+    )
     private List<Reaction> reactions = new ArrayList<>();
+
+    protected Statement() {
+        // Make JPA happy
+    }
 
     Statement(StatementId id) {
         this.id = Objects.requireNonNull(id);
         this.registerEvent(new StatementCreated(id));
     }
 
+    @PostLoad
+    void postLoad() {
+        // In case someone did something funny in the database
+        JMoleculesJpa.verifyNullability(this);
+    }
+
+    @PrePersist
+    void prePersist() {
+        // In case someone did something funny with reflection --- the rest is caught by nullaway
+        JMoleculesJpa.verifyNullability(this);
+    }
+
     void reacted(Reaction reaction) {
+        // TODO only allow if state is OPEN
+        if (!State.OPEN.equals(state)) {
+            throw new IllegalArgumentException();
+        }
         this.reactions.add(Objects.requireNonNull(reaction));
         this.registerEvent(new ReactionAdded(id, reaction.getId()));
     }
@@ -49,7 +88,10 @@ public class Statement
         return Collections.unmodifiableList(reactions);
     }
 
-    public record StatementId(LocalDate date, Type type, Integer sequenceOfDay) implements Identifier {
+    @Embeddable
+    public record StatementId(LocalDate date,
+                              @Enumerated(EnumType.STRING) Type type,
+                              Integer sequenceOfDay) implements Identifier {
 
         public static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("yyyyMMdd");
 
@@ -60,9 +102,15 @@ public class Statement
         }
     }
 
-    public record StatementCreated(StatementId statementId) implements DomainEvent {}
+    public record StatementCreated(StatementId statementId) implements DomainEvent {
+    }
 
-    public record ReactionAdded(StatementId statementId, ReactionId reactionId) implements DomainEvent {}
+    public record ReactionAdded(StatementId statementId, ReactionId reactionId) implements DomainEvent {
+    }
+
+    @Embeddable
+    public record AuthorId(String value) implements ValueObject {
+    }
 
     public enum State {
         OPEN, CLOSED
