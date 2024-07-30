9edeb9b6d9e5775535e09597aff41a1eeb403690
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2024-07-30 21:05:04 +0200

    Use RestClient.Builder as it comes with default from Spring Boot such as instrumentation for tracing

--- src/main/java/li/selman/ddd/statement/post/PostClientConfig.java
+++ src/main/java/li/selman/ddd/statement/post/PostClientConfig.java
@@ -2,6 +2,7 @@ package li.selman.ddd.statement.post;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -10,9 +11,15 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 class PostClientConfig {
 
   @Bean
-  PostClient postClient() {
-    var restClient = RestClient.create("https://jsonplaceholder.typicode.com");
-    var factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+  PostClient postClient(RestClient.Builder restClientBuilder) {
+    // https://docs.spring.io/spring-boot/reference/io/rest-client.html#io.rest-client.restclient
+    RestClient restClient =
+        restClientBuilder
+            .baseUrl("https://jsonplaceholder.typicode.com")
+            .requestFactory(new HttpComponentsClientHttpRequestFactory())
+            .build();
+    HttpServiceProxyFactory factory =
+        HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
     return factory.createClient(PostClient.class);
   }
 }
