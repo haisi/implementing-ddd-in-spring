1511a0d0da1908ff2b894d0b1df255441669532d
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2024-08-01 15:03:34 +0200

    Improve rest exception handling by adding traceIds and timestamps to all exceptions

--- src/main/java/li/selman/ddd/rest/RestExceptionHandler.java
+++ src/main/java/li/selman/ddd/rest/RestExceptionHandler.java
@@ -3,25 +3,71 @@ package li.selman.ddd.rest;
 import java.net.URI;
 import java.time.Instant;
 
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
+import org.springframework.http.*;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-class RestExceptionHandler {
+class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
   private static final Logger log = LoggerFactory.getLogger(RestExceptionHandler.class);
 
+  private final Tracer tracer;
+
+  RestExceptionHandler(Tracer tracer) {
+    this.tracer = tracer;
+  }
+
+  private void addAdditionalDetails(ProblemDetail problemDetail) {
+    problemDetail.setProperty("timestamp", Instant.now());
+    addTraceInfos(problemDetail);
+  }
+
+  private void addTraceInfos(ProblemDetail problemDetail) {
+    Span span = tracer.currentSpan();
+    if (span != null) {
+      problemDetail.setProperty("traceId", span.context().traceId());
+      problemDetail.setProperty("spanId", span.context().spanId());
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>Springs default exception handlers already create ProblemDetails. Here, we extend Springs
+   * ProblemDetails with additional details such as timestamps and trace ids.
+   */
+  @Override
+  protected ResponseEntity<Object> handleExceptionInternal(
+          Exception ex,
+          @Nullable Object body,
+          HttpHeaders headers,
+          HttpStatusCode statusCode,
+          WebRequest request) {
+    ResponseEntity<Object> objectResponseEntity =
+            super.handleExceptionInternal(ex, body, headers, statusCode, request);
+    if (objectResponseEntity != null && objectResponseEntity.hasBody()) {
+      if (objectResponseEntity.getBody() instanceof ProblemDetail) {
+        addAdditionalDetails((ProblemDetail) objectResponseEntity.getBody());
+      }
+    }
+
+    return objectResponseEntity;
+  }
+
   @ExceptionHandler(ResourceNotFoundException.class)
   ProblemDetail handleResourceNotFoundException(ResourceNotFoundException ex) {
-    ProblemDetail problemDetail =
-        ProblemDetail.forStatusAndDetail(ex.getStatusCode(), ex.getMessage());
+    var problemDetail = ProblemDetail.forStatusAndDetail(ex.getStatusCode(), ex.getMessage());
     problemDetail.setProperty("errorCode", "1");
-    problemDetail.setProperty("timestamp", Instant.now());
+    addAdditionalDetails(problemDetail);
     return problemDetail;
   }
 
@@ -31,6 +77,10 @@ class RestExceptionHandler {
     var problemDetail = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
     problemDetail.setProperty("errorCode", "0");
     problemDetail.setInstance(URI.create(request.getRequestURI()));
+    problemDetail.setDetail(ex.getMessage());
+
+    addAdditionalDetails(problemDetail);
+
     return problemDetail;
   }
 }
