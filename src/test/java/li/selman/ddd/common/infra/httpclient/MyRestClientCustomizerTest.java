cfee3624ed81d33fb9f26c444875319b587d1e1f
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2025-01-25 15:26:05 +0100

    Get test client working -- puh

---
+++ src/test/java/li/selman/ddd/common/infra/httpclient/MyRestClientCustomizerTest.java
@@ -0,0 +1,79 @@
+package li.selman.ddd.common.infra.httpclient;
+
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureMockRestServiceServer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+
+/**
+ * Tests whether:
+ * <ul>
+ *     <li>Logging works</li>
+ *     <li>in-process retry works</li>
+ *     <li>Exception wrapping works</li>
+ *     <li>Observability</li>
+ *     <li>Tracing</li>
+ *     <li>Metrics</li>
+ * </ul>
+ */
+@SpringBootTest
+@AutoConfigureMockRestServiceServer
+class MyRestClientCustomizerTest {
+
+    private TestClient client;
+
+    @Autowired
+    private MockRestServiceServer server;
+
+    @Autowired
+    RestClient.Builder restClientBuilder;
+
+    @BeforeEach
+    void setUp() {
+        server = MockRestServiceServer.bindTo(restClientBuilder).build(); // make the myClient use the mock
+
+        RestClient restClient = restClientBuilder.build();
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory //
+                .builderFor(RestClientAdapter.create(restClient)) //
+                .build();
+        client = factory.createClient(TestClient.class);
+    }
+
+    @Test
+    void test() {
+        @Language("JSON")
+        String postJson =
+                """
+                  {
+                    "author": "test",
+                    "value": "Mein Kommentar"
+                  }
+                  """;
+        server.expect(requestTo("/comments")).andRespond(withSuccess(postJson, APPLICATION_JSON));
+
+        // when then
+        TestClient.Comment body = client.findAll();
+//        var body = restClientBuilder.build().get().uri("/comments").retrieve().body(TestClient.Comment.class);
+
+        System.out.println(body);
+    }
+}
+
+interface TestClient {
+    @GetExchange("/comments")
+    Comment findAll();
+
+    record Comment(String author, String value) {}
+}
