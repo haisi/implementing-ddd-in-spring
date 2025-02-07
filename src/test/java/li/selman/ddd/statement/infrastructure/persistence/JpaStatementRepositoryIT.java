49a1a175ba2f5aaf97fb7733f0c158bdedc55e2e
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2025-02-07 19:39:45 +0100

    Add sample jpa repo it test

--- src/test/java/li/selman/ddd/statement/infrastructure/persistence/JpaStatementRepositoryIT.java
+++ src/test/java/li/selman/ddd/statement/infrastructure/persistence/JpaStatementRepositoryIT.java
@@ -1,5 +1,29 @@
 package li.selman.ddd.statement.infrastructure.persistence;
 
-import static org.junit.jupiter.api.Assertions.*;
+import li.selman.ddd.common.container.EnablePostgresContainer;
+import li.selman.ddd.statement.Statement;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
-class JpaStatementRepositoryIT {}
+@DataJpaTest(
+        includeFilters =
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JpaStatementRepository.class))
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@EnablePostgresContainer
+class JpaStatementRepositoryIT {
+
+    // https://stackoverflow.com/a/68890310 prefer the initalizer approach
+
+    @Autowired
+    JpaStatementRepository jpaStatementRepository;
+
+    @Test
+    void findAll() {
+        jpaStatementRepository.findByState(Statement.State.CLOSED);
+    }
+
+}
