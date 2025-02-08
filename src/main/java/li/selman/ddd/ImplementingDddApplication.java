07063be175fb4dd2867e60759469709dba868edb
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2025-02-08 18:40:06 +0100

    Remove superfluous @SpringBootApp

--- src/main/java/li/selman/ddd/ImplementingDddApplication.java
+++ src/main/java/li/selman/ddd/ImplementingDddApplication.java
@@ -1,11 +1,9 @@
 package li.selman.ddd;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.modulith.Modulith;
 
-@SpringBootApplication
 @Modulith
 @ConfigurationPropertiesScan
 public class ImplementingDddApplication {
