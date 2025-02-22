e1d4abcf3c34e22c20e48b58f17b939a27464f72
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2025-02-22 10:55:37 +0100

    Add Spring Data Configuration

---
+++ src/main/java/li/selman/ddd/common/SpringDataConfig.java
@@ -0,0 +1,12 @@
+package li.selman.ddd.common;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "li.selman.ddd")
+@EntityScan("li.selman.ddd")
+public class SpringDataConfig {}
