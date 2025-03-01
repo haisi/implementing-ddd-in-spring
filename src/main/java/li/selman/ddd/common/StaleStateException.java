8bb0a6e6514391900cac2e3a6931e9cf9fbcc556
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2025-03-01 13:00:52 +0100

    StaleStateException extend from MyException

--- src/main/java/li/selman/ddd/common/StaleStateException.java
+++ src/main/java/li/selman/ddd/common/StaleStateException.java
@@ -1,11 +1,22 @@
 package li.selman.ddd.common;
 
+import li.selman.ddd.common.error.MyException;
+import li.selman.ddd.common.error.StandardErrorKey;
+import li.selman.ddd.common.error.StandardTemporality;
 import org.jmolecules.ddd.types.Identifier;
+import org.springframework.http.HttpStatus;
 
-public class StaleStateException extends RuntimeException {
+import java.util.Map;
+
+public class StaleStateException extends MyException {
 
     private StaleStateException(String id) {
-        super(String.format("Aggregate of id %s is stale", id));
+        super(
+                String.format("Aggregate of id %s is stale", id),
+                StandardErrorKey.PROCESS_CONFLICT,
+                StandardTemporality.TEMPORARY_GLOBAL,
+                HttpStatus.CONFLICT,
+                Map.of("object-id", id));
     }
 
     public static StaleStateException forAggregate(Identifier identifier) {
