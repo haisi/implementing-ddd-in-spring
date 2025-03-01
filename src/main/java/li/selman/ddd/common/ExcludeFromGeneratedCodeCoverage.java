a1a9fa1eee733fff5744753e7f7e61f75076005f
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2025-03-01 12:16:29 +0100

    Add annotation to exclude class from Jacoco coverage

---
+++ src/main/java/li/selman/ddd/common/ExcludeFromGeneratedCodeCoverage.java
@@ -0,0 +1,15 @@
+package li.selman.ddd.common;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Jacoco ignores code annotated with an annotation containing "Generated".
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.METHOD, ElementType.TYPE, ElementType.CONSTRUCTOR })
+public @interface ExcludeFromGeneratedCodeCoverage {
+    String reason() default "";
+}
